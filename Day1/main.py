c7c67f26cb5e898660ec38b93826a93de84ca03e
MattiTheKatti <dragonmatte@hotmail.com>
2024-02-17 23:09:48 +0100

    Completed Day 1 part 1

--- Day1/main.py
+++ Day1/main.py
@@ -1,1 +1,17 @@
+def getCalVal(text):
+    firstNum = ""
+    lastNum = ""
+    for letter in text:
+        if letter.isnumeric():
+            if firstNum == "":
+                firstNum = letter
+            lastNum = letter
+    return int(firstNum + lastNum)
 
+result = 0
+
+f = open("AdventOfCode/Day1/input.txt", "r")
+for x in f:
+    result += getCalVal(x)
+
+print(result)
